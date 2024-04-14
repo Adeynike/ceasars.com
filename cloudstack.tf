208be9cc4f3c0be41c0137297441813173a4a682
Adeniji Kehinde David <adenijikehinde00@gmail.com>
2024-04-14 16:51:01 +0100

    third commits

--- cloudstack.tf
+++ cloudstack.tf
@@ -1,4 +1,5 @@
 resource "aws_cloudformation_stack" "stack" {
-  name = "s3tablestack"
+  name         = "s3tablestack"
   template_body = file("CloudFormationTemplates/s3.yaml")
+  capabilities = ["CAPABILITY_IAM"]
 }
