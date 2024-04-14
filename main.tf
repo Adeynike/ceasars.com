013acdc2d318151e0571110287c0a70e67ec443b
Adeniji Kehinde David <adenijikehinde00@gmail.com>
2024-04-14 16:28:58 +0100

    third commits

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "tfbucket"
-    key            = "terraform.tfstate"
-    dynamodb_table = "tftable"
-    region         = "us-east-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "tfbucket"
+#     key            = "terraform.tfstate"
+#     dynamodb_table = "tftable"
+#     region         = "us-east-1"
+#   }
+# }
